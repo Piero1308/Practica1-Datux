fdbe792be7d55eb5eb7c6b95932f7dbe00e01f29
Piero Beretta <131180867+Piero1308@users.noreply.github.com>
2025-01-21 04:05:10 +0000

    EjerciciosPractica1

---
+++ Ejercicio6.py
@@ -0,0 +1,5 @@
+edad=int(input("Ingresa tu edad "))
+if edad>=18:
+    print("USTED ES MAYOR DE EDAD, FELICIDADEEEES!!!")
+else: 
+    print("AUN TE FALTA CRECER")    
---
+++ Ejercicio8.py
@@ -0,0 +1,4 @@
+
+n = int(input("Ingresa un número entero: "))
+suma = n * (n + 1) // 2
+print(f"La suma de los números de 1 a {n} es: {suma}")
